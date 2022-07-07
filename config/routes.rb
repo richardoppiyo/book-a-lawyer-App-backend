ca774e29d5805a9222f1cb7cf139dfe5d271e953
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-07 23:43:16 +0300

    add configuration

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1, defaults: { format: 'json' } do
-      resources :reservations
+      resources :reservations, except: :show
       resources :lawyers
     end
   end
