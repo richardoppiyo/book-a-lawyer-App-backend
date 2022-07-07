bafbcba16286e76e4a1598ac50a021016a87a723
Kenny <kennysalazar3197@gmail.com>
2022-07-07 14:23:46 -0500

    Add associations and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :reservations, dependent: :destroy
+
+  validates_associated :reservations
+  validates :name, presence: true, length: {maximum: 50}
+  validates :phone, presence: true, length: {maximum: 15}, numericality: { only_integer: true }
 end
