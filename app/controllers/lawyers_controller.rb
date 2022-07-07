47b4d6c70615e3d22a1eec10fa64d5d7c5a8e5dd
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-07 21:00:33 +0300

    solve rubocop issues

--- app/controllers/lawyers_controller.rb
+++ app/controllers/lawyers_controller.rb
@@ -1,37 +1,36 @@
 class LawyersController < ApplicationController
-    def index
-        @lawyer = Lawyer.all
-    end
+  def index
+    @lawyer = Lawyer.all
+  end
 
-    def create
-        @current_user = current_user
-        @lawyer = current_user.Lawyer.new(lawyer_params)
-        @lawyer.save
-        redirect_to lawyers_path
-    end
-    
-      def update
-        @current_user = current_user
-        @lawyer = current_user.Lawyer.new.find(params[:id])
-        @lawyer.update(lawyer_params)
-        redirect_to lawyers_path
-      end
-    
-      def show
-        @lawyer = Lawyer.find(params[:id])
-      end
-    
-      def destroy
-        @current_user = current_user
-        @lawyer = current_user.Lawyer.new.find(params[:id])
-        @lawyer.destroy
-        redirect_to lawyers_path
-      end
-    
-      private
-    
-      def lawyer_params
-        params.require(:lawyer).permit(:name, :phone, :email, :location, :rates, :bio)
-      end
+  def create
+    @current_user = current_user
+    @lawyer = current_user.Lawyer.new(lawyer_params)
+    @lawyer.save
+    redirect_to lawyers_path
+  end
+
+  def update
+    @current_user = current_user
+    @lawyer = current_user.Lawyer.new.find(params[:id])
+    @lawyer.update(lawyer_params)
+    redirect_to lawyers_path
+  end
+
+  def show
+    @lawyer = Lawyer.find(params[:id])
+  end
+
+  def destroy
+    @current_user = current_user
+    @lawyer = current_user.Lawyer.new.find(params[:id])
+    @lawyer.destroy
+    redirect_to lawyers_path
+  end
+
+  private
+
+  def lawyer_params
+    params.require(:lawyer).permit(:name, :phone, :email, :location, :rates, :bio)
+  end
 end
-  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,28 @@
 class UsersController < ApplicationController
-    def index
+  def index; end
 
-    end
+  def create
+    @current_user = current_user
+    @user = current_user.User.new(user_params)
+    @user.save
+  end
 
-    def create
-        @current_user = current_user
-        @user = current_user.User.new(user_params)
-        @user.save
-    end
-    
-      def update
-        @current_user = current_user
-        @user = current_user.User.new.find(params[:id])
-        @user.update(inventory_food_params)
-      end
-    
-    
-      def destroy
-        @current_user = current_user
-        @user = current_user.User.new.find(params[:id])
-        @user.destroy
-        redirect_to inventory_path(@inventory_food.inventory)
-      end
-    
-      private
-    
-      def user_params
-        params.require(:user).permit(:name, :phone, :quantity, :email)
-      end
+  def update
+    @current_user = current_user
+    @user = current_user.User.new.find(params[:id])
+    @user.update(inventory_food_params)
+  end
+
+  def destroy
+    @current_user = current_user
+    @user = current_user.User.new.find(params[:id])
+    @user.destroy
+    redirect_to inventory_path(@inventory_food.inventory)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :phone, :quantity, :email)
+  end
 end
-  
