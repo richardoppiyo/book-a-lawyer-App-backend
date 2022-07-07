a09877df16ac6302838e7edde1f635f3c4c6eda1
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-07 22:36:15 +0300

    add api namespace on route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :reservations
-  resources :lawyers
+  # resources :reservations
+  # resources :lawyers
+
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      resources :reservations
+      resources :lawyers
+    end
+  end
 
   # Defines the root path route ("/")
   root "lawyers#index"
