f4df0764c0007927e5b567d775691d1f7b4f76de
Kenny <kennysalazar3197@gmail.com>
2022-07-07 14:33:02 -0500

    Add associations and validations to Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,9 @@
 class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :lawyer
+
+  validates :date, presence: true
+  validates :is_active, inclusion: [true, false]
+  validates :duration, comparison: { greater_than: 0 }, format: { with: /\A\d+(?:\.\d{0,2})?\z/,
+                                                                  message: 'max two decimal positions' }
 end
