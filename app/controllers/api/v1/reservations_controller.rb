7a599b92bcf6de4c87fb8fe1184e6456f3ac91bf
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-07 22:49:40 +0300

    modify reservation controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -2,24 +2,31 @@ class ReservationsController < ApplicationController
   def index
     @current_user = current_user
     @reservation = current_user.Reservation.all
+    render json: { reservation: @reservation }
   end
 
   def create
     @reservation = Reservation.new(reservation_params)
-    @reservation.save
-    redirect_to reservations_path
+    if @reservation.save
+      render json: { result: 'Reservation successful' }
+    else
+      render json: { result: 'Something went wrong' }, status: :unprocessable_entity
+    end
   end
 
   def update
     @reservation = Reservation.new.find(params[:id])
-    @reservation.update(reservation_params)
-    redirect_to reservations_path
+    if @reservation.update(reservation_params)
+      render json: { result: 'Reservation updated successfully' }
+    else
+      render json: { result: 'Something went wrong' }, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @reservation = Reservation.new.find(params[:id])
     @reservation.destroy
-    redirect_to reservations_path
+    render json: { result: 'Reservation cancelled successfully' }
   end
 
   private
