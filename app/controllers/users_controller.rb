5aa69bb5dde4574d6e81ce69af613af0755521c7
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-07 20:12:16 +0300

    add actions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,30 @@ class UsersController < ApplicationController
     def index
 
     end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        redirect_to inventory_path(@inventory_food.inventory)
+    end
+    
+      def update
+        @user = User.new.find(params[:id])
+        @user.update(inventory_food_params)
+        redirect_to inventory_path(@inventory_food.inventory)
+      end
+    
+    
+      def destroy
+        @user = User.new.find(params[:id])
+        @user.destroy
+        redirect_to inventory_path(@inventory_food.inventory)
+      end
+    
+      private
+    
+      def user_params
+        params.require(:user).permit(:name, :phone, :quantity, :email)
+      end
 end
   
