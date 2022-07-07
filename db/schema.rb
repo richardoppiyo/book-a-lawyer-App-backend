5ddc31ddf1a01d6cc6200c9a8b33d2d769d2e0af
Richard Opiyo <richardoppiyo@gmail.com>
2022-07-07 18:51:20 +0300

    add refrence  to reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_07_153057) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_154507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "reservationdate"
+    t.boolean "is_active"
+    t.integer "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_07_153057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "users"
 end
