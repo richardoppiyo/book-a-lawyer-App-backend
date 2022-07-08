ddde5c78b83c3e2118e65a854b98804ede9566f9
Kenny <kennysalazar3197@gmail.com>
2022-07-07 23:56:20 -0500

    Update devise routes and create registrations controllers

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,18 @@
+class RegistrationsController < Devise::RegistrationsController
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    if resource.persisted?
+      if resource.active_for_authentication?
+        sign_up(resource_name, resource)
+        render json: { result: 'success' }
+      else
+        expire_data_after_sign_in!
+        render json: { result: 'success_with_notice', notice: "signed_up_but_#{resource.inactive_message}" }
+      end
+    else
+      render json: { result: 'failed', errors: resource.errors }, status: :unprocessable_entity
+    end
+  end
+end
