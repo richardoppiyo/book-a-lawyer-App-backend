5531a1d8ac1417cce1ae949d6cb94c2b84cfb90c
Kenny <kennysalazar3197@gmail.com>
2022-07-07 21:42:03 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { edit: 'account' }, defaults: { format: :json },
+                               controllers: { registrations: 'registrations', sessions: 'sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # resources :reservations
   # resources :lawyers
