fc0f6ae3c4bb497911ba81d7f610822281564553
Kenny <kennysalazar3197@gmail.com>
2022-07-08 11:52:42 -0500

    Refactor sessions#destroy to integrate API jwt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,18 @@ class SessionsController < Devise::SessionsController
     sign_in(resource_name, resource)
     render json: { result: 'success' }
   end
+
+  def destroy
+    render json: { result: 'success' }
+  end
+
+  private
+
+  def verify_signed_out_user
+    missing_auth_token && return unless request.headers['Authorization'].present?
+  end
+
+  def missing_auth_token
+    render json: { result: 'failed', error: 'missing_auth_token' }, status: :unauthorized
+  end
 end
