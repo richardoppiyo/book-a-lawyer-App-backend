e7f52b8493bacdce17169ba1a65b6981d89c3c31
Kenny <kennysalazar3197@gmail.com>
2022-07-10 15:32:18 -0500

    Update auth controllers to include avatar url in response

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,11 @@ class RegistrationsController < Devise::RegistrationsController
     if resource.persisted?
       if resource.active_for_authentication?
         sign_up(resource_name, resource)
-        render json: { result: 'success', user: resource }
+        render json: { result: 'success', user: {
+          **resource.attributes
+            .except('encrypted_password', 'reset_password_token', 'reset_password_sent_at', 'remember_created_at'),
+          avatar: resource.avatar.present? ? url_for(resource.avatar) : nil
+        } }
       else
         expire_data_after_sign_in!
         render json: { result: 'success_with_notice', notice: "signed_up_but_#{resource.inactive_message}" }
@@ -23,7 +27,11 @@ class RegistrationsController < Devise::RegistrationsController
     if resource_updated
       current_user.avatar.purge if account_update_params[:remove_avatar]
 
-      render json: { result: 'success', user: resource }
+      render json: { result: 'success', user: {
+        **resource.attributes
+          .except('encrypted_password', 'reset_password_token', 'reset_password_sent_at', 'remember_created_at'),
+        avatar: resource.avatar.present? ? url_for(resource.avatar) : nil
+      } }
     else
       render json: { result: 'failed', error: resource.errors }, status: :unprocessable_entity
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,11 @@ class SessionsController < Devise::SessionsController
   def create
     self.resource = warden.authenticate!(auth_options)
     sign_in(resource_name, resource)
-    render json: { result: 'success', user: resource }
+    render json: { result: 'success', user: {
+      **resource.attributes
+        .except('encrypted_password', 'reset_password_token', 'reset_password_sent_at', 'remember_created_at'),
+      avatar: resource.avatar.present? ? url_for(resource.avatar) : nil
+    } }
   end
 
   def destroy
